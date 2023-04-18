0cd309774ad5eb72cf933190911e062c2f3f23c6
Onur Karakaslar <onurkara3594@gmail.com>
2023-04-18 15:55:32 +0200

    Update preprocessing_cmd.sh
    example, cellSNP-lite compatibility

--- example/preprocessing_cmd.sh
+++ example/preprocessing_cmd.sh
@@ -14,8 +14,7 @@ cellsnp-lite -S bam.lst -i bam.lst -o cellsnp --UMItag None --genotype --gzip --
 
 #run cellsnp-lite on the .bam directly
 #change --chrom= to whatever reference genome you aligned to, in most cases 10x data are aligned to GrCh38 so the chr name is MT
-cellsnp-lite -s possorted_genome.bam -b barcodes.tsv -o cellsnp --chrom=MT --UMItag Auto --minMAF 0 --minCOUNT 0 --genotype --gzip -p 10
-
+cellsnp-lite -s possorted_genome.bam -b barcodes.tsv -o cellsnp --chrom M --UMItag Auto --minMAF 0 --minCOUNT 0 --genotype --gzip -p 10
 
 #The above steps should generate a cell x snp .vcf file (cellSNP.cells.vcf.gz), or AD/DP sparse matrices if you did not use the --genotype option
 
